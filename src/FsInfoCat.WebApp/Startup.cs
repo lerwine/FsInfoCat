f8bde3c01e09444c0e126b355327526a66bdcfd9
Leonard T. Erwine <lenny@erwinefamily.net>
2020-12-23 02:38:44 -0500

    Added cookie based login

--- src/FsInfoCat.WebApp/Startup.cs
+++ src/FsInfoCat.WebApp/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,12 +13,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 
 namespace FsInfoCat.WebApp
 {
     public class Startup
     {
+        public const string CookieScheme = "FsInfoCat";
+        public const string SettingsKey_ConnectionString = "FsInfoCat";
+        public const string SettingsKey_DBPassword = "DBPassword";
+        public const string UrlPath_ExceptionHandler = "/index.html#!/error";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,8 +37,20 @@ namespace FsInfoCat.WebApp
         {
             services.AddControllers();
 
-            string connectionString = Configuration.GetConnectionString("FsInfoCat");
-            string pwd = Configuration["DBPassword"];
+            string cookieScheme = Configuration[CookieScheme];
+            services.AddAuthentication(cookieScheme) // Sets the default scheme to cookies
+                .AddCookie(cookieScheme, options =>
+                {
+                    options.AccessDeniedPath = "/account/denied";
+                    options.LoginPath = "/account/login";
+                });
+
+            // Example of how to customize a particular instance of cookie options and
+            // is able to also use other services.
+            services.AddSingleton<IConfigureOptions<CookieAuthenticationOptions>, ConfigureFsInfoCatCookie>();
+
+            string connectionString = Configuration.GetConnectionString(SettingsKey_ConnectionString);
+            string pwd = Configuration[SettingsKey_DBPassword];
             if (!String.IsNullOrEmpty(pwd))
             {
                 var builder = new SqlConnectionStringBuilder(connectionString);
@@ -49,7 +69,7 @@ namespace FsInfoCat.WebApp
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
-                app.UseExceptionHandler("/index.html#!/error");
+                app.UseExceptionHandler(UrlPath_ExceptionHandler);
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
