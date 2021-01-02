1f23c317f350a87a8f112a2866ae7d7a0b80fa2c
Leonard T. Erwine <lenny@erwinefamily.net>
2021-01-02 11:05:33 -0500

    Added verbose and debug output to module load

--- src/FsInfoCat.PS/Test/TestModule.ps1
+++ src/FsInfoCat.PS/Test/TestModule.ps1
@@ -1,4 +1,4 @@
-Import-Module -Name (($PSScriptRoot | Join-Path -ChildPath '..\..\..\Debug\Desktop\FsInfoCat')) -ErrorAction Stop;
+Import-Module -Name (($PSScriptRoot | Join-Path -ChildPath '..\..\..\Debug\Desktop\FsInfoCat')) -ErrorAction Stop -Verbose -Debug -InformationAction Continue -WarningAction Continue;
 
 $PwHash = [FsInfoCat.PwHash]::Import('XrVxbJmeSwCzNtQp6PpepBE8WUh1nfhPGlQCBoIOaS6ho6+7UH/lGnJgH0w2hF2oFzxDrQ6liVHWDldtFawAetBTWV+jCsFG');
 if ($null -eq $PwHash) {
