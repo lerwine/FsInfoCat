3ec90c32865b8cb87a21bcb3f817ca817baeb2ff
Leonard T. Erwine <lenny@erwinefamily.net>
2021-01-04 16:52:29 -0500

    Made property names more descriptive

--- src/FsInfoCat/Util/PwHash.cs
+++ src/FsInfoCat/Util/PwHash.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -18,57 +19,167 @@ namespace FsInfoCat.Util
         private const int FIELD_OFFSET_7 = 56;
         public const int HASH_BYTES_LENGTH = 64;
         public const int SALT_BYTES_LENGTH = 8;
-        public const int TOTAL_LENGTH = HASH_BYTES_LENGTH + SALT_BYTES_LENGTH;
+        public const int TOTAL_BYTES_LENGTH = HASH_BYTES_LENGTH + SALT_BYTES_LENGTH;
 
         [FieldOffset(0)]
-        private readonly ulong _hashBits0;
+        private readonly ulong _hashBits000_03f;
 
         [FieldOffset(FIELD_OFFSET_1)]
-        private readonly ulong _hashBits1;
+        private readonly ulong _hashBits040_07f;
 
         [FieldOffset(FIELD_OFFSET_2)]
-        private readonly ulong _hashBits2;
+        private readonly ulong _hashBits080_0bf;
 
         [FieldOffset(FIELD_OFFSET_3)]
-        private readonly ulong _hashBits3;
+        private readonly ulong _hashBits0c0_0ff;
 
         [FieldOffset(FIELD_OFFSET_4)]
-        private readonly ulong _hashBits4;
+        private readonly ulong _hashBits100_13f;
 
         [FieldOffset(FIELD_OFFSET_5)]
-        private readonly ulong _hashBits5;
+        private readonly ulong _hashBits140_17f;
 
         [FieldOffset(FIELD_OFFSET_6)]
-        private readonly ulong _hashBits6;
+        private readonly ulong _hashBits180_1bf;
 
         [FieldOffset(FIELD_OFFSET_7)]
-        private readonly ulong _hashBits7;
+        private readonly ulong _hashBits1c0_1ff;
 
         [FieldOffset(HASH_BYTES_LENGTH)]
         private readonly ulong _saltBits;
 
-        public ulong HashBits0 => _hashBits0;
-        public ulong HashBits1 => _hashBits1;
-        public ulong HashBits2 => _hashBits2;
-        public ulong HashBits3 => _hashBits3;
-        public ulong HashBits4 => _hashBits4;
-        public ulong HashBits5 => _hashBits5;
-        public ulong HashBits6 => _hashBits6;
-        public ulong HashBits7 => _hashBits7;
+        public ulong HashBits000_03f => _hashBits000_03f;
+        public ulong HashBits040_07f => _hashBits040_07f;
+        public ulong HashBits080_0bf => _hashBits080_0bf;
+        public ulong HashBits0c0_0ff => _hashBits0c0_0ff;
+        public ulong HashBits100_13f => _hashBits100_13f;
+        public ulong HashBits140_17f => _hashBits140_17f;
+        public ulong HashBits180_1bf => _hashBits180_1bf;
+        public ulong HashBits1c0_1ff => _hashBits1c0_1ff;
         public ulong SaltBits => _saltBits;
 
-        private PwHash(byte[] sha512Hash, ulong salt)
+        public PwHash(ulong hashBits000_03f, ulong hashBits040_07f, ulong hashBits080_0bf, ulong hashBits0c0_0ff, ulong hashBits100_13f, ulong hashBits140_17f, ulong hashBits180_1bf,
+            ulong hashBits1c0_1ff, ulong saltBits)
         {
-            _hashBits0 = BitConverter.ToUInt64(sha512Hash, 0);
-            _hashBits1 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_1);
-            _hashBits2 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_2);
-            _hashBits3 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_3);
-            _hashBits4 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_4);
-            _hashBits5 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_5);
-            _hashBits6 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_6);
-            _hashBits7 = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_7);
-            _saltBits = salt;
+            _hashBits000_03f = hashBits000_03f;
+            _hashBits040_07f = hashBits040_07f;
+            _hashBits080_0bf = hashBits080_0bf;
+            _hashBits0c0_0ff = hashBits0c0_0ff;
+            _hashBits100_13f = hashBits100_13f;
+            _hashBits140_17f = hashBits140_17f;
+            _hashBits180_1bf = hashBits180_1bf;
+            _hashBits1c0_1ff = hashBits1c0_1ff;
+            _saltBits = saltBits;
         }
+
+        public PwHash(IEnumerable<byte> sha512HashBytes, int hashStartIndex, IEnumerable<byte> saltBytes, int saltStartIndex)
+        {
+            if (null == sha512HashBytes)
+                throw new ArgumentNullException("sha512HashBytes");
+            if (null == saltBytes)
+                throw new ArgumentNullException("saltBytes");
+            if (hashStartIndex < 0)
+                throw new ArgumentOutOfRangeException("hashStartIndex");
+            if (saltStartIndex < 0)
+                throw new ArgumentOutOfRangeException("saltStartIndex");
+            byte[] hash;
+            if (sha512HashBytes is byte[])
+                hash = (byte[])sha512HashBytes;
+            else
+            {
+                hash = sha512HashBytes.Skip(hashStartIndex).Take(HASH_BYTES_LENGTH).ToArray();
+                hashStartIndex = 0;
+            }
+            if (hash.Length < (HASH_BYTES_LENGTH + hashStartIndex))
+                throw new ArgumentException("SHA512 hash array must contain at least" + HASH_BYTES_LENGTH.ToString() + " bytes after the start index.", "sha512HashBytes");
+            byte[] salt;
+            if (saltBytes is byte[])
+                salt = (byte[])saltBytes;
+            else
+            {
+                salt = saltBytes.Skip(saltStartIndex).Take(SALT_BYTES_LENGTH).ToArray();
+                saltStartIndex = 0;
+            }
+            if (salt.Length < (SALT_BYTES_LENGTH + saltStartIndex))
+                throw new ArgumentException("Salt array must contain at least" + SALT_BYTES_LENGTH.ToString() + " bytes after the start index.", "saltBytes");
+            _hashBits000_03f = BitConverter.ToUInt64(hash, hashStartIndex);
+            _hashBits040_07f = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_1);
+            _hashBits080_0bf = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_2);
+            _hashBits0c0_0ff = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_3);
+            _hashBits100_13f = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_4);
+            _hashBits140_17f = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_5);
+            _hashBits180_1bf = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_6);
+            _hashBits1c0_1ff = BitConverter.ToUInt64(hash, hashStartIndex + FIELD_OFFSET_7);
+            _saltBits = BitConverter.ToUInt64(salt, saltStartIndex);
+        }
+
+        public PwHash(IEnumerable<byte> hashAndSaltBytes, int startIndex)
+        {
+            if (null == hashAndSaltBytes)
+                throw new ArgumentNullException("hashAndSaltBytes");
+            if (startIndex < 0)
+                throw new ArgumentOutOfRangeException("startIndex");
+            byte[] data;
+            if (hashAndSaltBytes is byte[])
+                data = (byte[])hashAndSaltBytes;
+            else
+            {
+                data = hashAndSaltBytes.Skip(startIndex).Take(TOTAL_BYTES_LENGTH).ToArray();
+                startIndex = 0;
+            }
+            if (data.Length < (TOTAL_BYTES_LENGTH +  startIndex))
+                throw new ArgumentException("Array must contain at least" + TOTAL_BYTES_LENGTH.ToString() + " bytes after the start index.", "hashAndSaltBytes");
+            _hashBits000_03f = BitConverter.ToUInt64(data, startIndex);
+            _hashBits040_07f = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_1);
+            _hashBits080_0bf = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_2);
+            _hashBits0c0_0ff = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_3);
+            _hashBits100_13f = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_4);
+            _hashBits140_17f = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_5);
+            _hashBits180_1bf = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_6);
+            _hashBits1c0_1ff = BitConverter.ToUInt64(data, startIndex + FIELD_OFFSET_7);
+            _saltBits = BitConverter.ToUInt64(data, startIndex + HASH_BYTES_LENGTH);
+        }
+
+        private PwHash(byte[] sha512Hash, byte[] saltBytes)
+        {
+            _hashBits000_03f = BitConverter.ToUInt64(sha512Hash, 0);
+            _hashBits040_07f = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_1);
+            _hashBits080_0bf = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_2);
+            _hashBits0c0_0ff = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_3);
+            _hashBits100_13f = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_4);
+            _hashBits140_17f = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_5);
+            _hashBits180_1bf = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_6);
+            _hashBits1c0_1ff = BitConverter.ToUInt64(sha512Hash, FIELD_OFFSET_7);
+            _saltBits = BitConverter.ToUInt64(saltBytes, 0);
+        }
+
+        private PwHash(byte[] hashAndSaltBytes)
+        {
+            _hashBits000_03f = BitConverter.ToUInt64(hashAndSaltBytes, 0);
+            _hashBits040_07f = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_1);
+            _hashBits080_0bf = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_2);
+            _hashBits0c0_0ff = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_3);
+            _hashBits100_13f = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_4);
+            _hashBits140_17f = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_5);
+            _hashBits180_1bf = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_6);
+            _hashBits1c0_1ff = BitConverter.ToUInt64(hashAndSaltBytes, FIELD_OFFSET_7);
+            _saltBits = BitConverter.ToUInt64(hashAndSaltBytes, HASH_BYTES_LENGTH);
+        }
+
+        public IEnumerable<byte> GetHashAndSaltBytes() => (new ulong[]
+        {
+            _hashBits000_03f, _hashBits040_07f, _hashBits080_0bf, _hashBits0c0_0ff,
+            _hashBits100_13f, _hashBits140_17f, _hashBits180_1bf, _hashBits1c0_1ff,
+            _saltBits
+        }).SelectMany(v => BitConverter.GetBytes(v));
+
+        public IEnumerable<byte> GetSha512HashBytes() => (new ulong[]
+        {
+            _hashBits000_03f, _hashBits040_07f, _hashBits080_0bf, _hashBits0c0_0ff,
+            _hashBits100_13f, _hashBits140_17f, _hashBits180_1bf, _hashBits1c0_1ff
+        }).SelectMany(v => BitConverter.GetBytes(v));
+
+        public byte[] GetSaltBytes() => BitConverter.GetBytes(_saltBits);
 
         private static byte[] ComputeHash(string rawPw, byte[] salt)
         {
@@ -86,9 +197,9 @@ namespace FsInfoCat.Util
             byte[] data;
             try { data = Convert.FromBase64String(base64EncodedHash); }
             catch (Exception exc) { throw new ArgumentException("Invalid base-64 string", "base64EncodedHash", exc); }
-            if (data.Length != TOTAL_LENGTH)
+            if (data.Length != TOTAL_BYTES_LENGTH)
                 throw new ArgumentException("Invalid data length", "base64EncodedHash");
-            return new PwHash(data, BitConverter.ToUInt64(data, HASH_BYTES_LENGTH));
+            return new PwHash(data);
         }
 
         public static PwHash? Create(string rawPw)
@@ -98,14 +209,15 @@ namespace FsInfoCat.Util
             byte[] salt = new byte[SALT_BYTES_LENGTH];
             using (RNGCryptoServiceProvider cryptoServiceProvider = new RNGCryptoServiceProvider())
                 cryptoServiceProvider.GetBytes(salt);
-            return new PwHash(ComputeHash(rawPw, salt), BitConverter.ToUInt64(salt, 0));
+            return new PwHash(ComputeHash(rawPw, salt), salt);
         }
 
-        public static PwHash? Create(string rawPw, ulong salt)
+        public static PwHash? Create(string rawPw, ulong saltBits)
         {
             if (string.IsNullOrEmpty(rawPw))
                 return null;
-            return new PwHash(ComputeHash(rawPw, BitConverter.GetBytes(salt)), salt);
+            byte[] saltBytes = BitConverter.GetBytes(saltBits);
+            return new PwHash(ComputeHash(rawPw, saltBytes), saltBytes);
         }
 
         public static bool Test(PwHash? hash, string rawPw)
@@ -117,27 +229,24 @@ namespace FsInfoCat.Util
         {
             if (string.IsNullOrEmpty(rawPw))
                 return false;
-            byte[] hash = ComputeHash(rawPw, BitConverter.GetBytes(_saltBits));
-            return BitConverter.ToUInt64(hash, 0) == _hashBits0 &&
-                BitConverter.ToUInt64(hash, FIELD_OFFSET_1) == _hashBits1 && BitConverter.ToUInt64(hash, FIELD_OFFSET_2) == _hashBits2 &&
-                BitConverter.ToUInt64(hash, FIELD_OFFSET_3) == _hashBits3 && BitConverter.ToUInt64(hash, FIELD_OFFSET_4) == _hashBits4 &&
-                BitConverter.ToUInt64(hash, FIELD_OFFSET_5) == _hashBits5 && BitConverter.ToUInt64(hash, FIELD_OFFSET_6) == _hashBits6 &&
-                BitConverter.ToUInt64(hash, FIELD_OFFSET_7) == _hashBits7;
+            byte[] hash = ComputeHash(rawPw, GetSaltBytes());
+            return BitConverter.ToUInt64(hash, 0) == _hashBits000_03f &&
+                BitConverter.ToUInt64(hash, FIELD_OFFSET_1) == _hashBits040_07f && BitConverter.ToUInt64(hash, FIELD_OFFSET_2) == _hashBits080_0bf &&
+                BitConverter.ToUInt64(hash, FIELD_OFFSET_3) == _hashBits0c0_0ff && BitConverter.ToUInt64(hash, FIELD_OFFSET_4) == _hashBits100_13f &&
+                BitConverter.ToUInt64(hash, FIELD_OFFSET_5) == _hashBits140_17f && BitConverter.ToUInt64(hash, FIELD_OFFSET_6) == _hashBits180_1bf &&
+                BitConverter.ToUInt64(hash, FIELD_OFFSET_7) == _hashBits1c0_1ff;
         }
 
         public override string ToString()
         {
-            return Convert.ToBase64String(BitConverter.GetBytes(_hashBits0).Concat(BitConverter.GetBytes(_hashBits1)).Concat(BitConverter.GetBytes(_hashBits2))
-                .Concat(BitConverter.GetBytes(_hashBits3)).Concat(BitConverter.GetBytes(_hashBits4)).Concat(BitConverter.GetBytes(_hashBits5))
-                .Concat(BitConverter.GetBytes(_hashBits6)).Concat(BitConverter.GetBytes(_hashBits7)).Concat(BitConverter.GetBytes(_saltBits))
-                .ToArray());
+            return Convert.ToBase64String(GetHashAndSaltBytes().ToArray());
         }
 
         public bool Equals(PwHash other)
         {
-            return _hashBits0 == other._hashBits0 && _hashBits1 == other._hashBits1 && _hashBits2 == other._hashBits2 &&
-                _hashBits3 == other._hashBits3 && _hashBits4 == other._hashBits4 && _hashBits5 == other._hashBits5 &&
-                _hashBits6 == other._hashBits6 && _hashBits7 == other._hashBits7 && _saltBits == other._saltBits;
+            return _hashBits000_03f == other._hashBits000_03f && _hashBits040_07f == other._hashBits040_07f && _hashBits080_0bf == other._hashBits080_0bf &&
+                _hashBits0c0_0ff == other._hashBits0c0_0ff && _hashBits100_13f == other._hashBits100_13f && _hashBits140_17f == other._hashBits140_17f &&
+                _hashBits180_1bf == other._hashBits180_1bf && _hashBits1c0_1ff == other._hashBits1c0_1ff && _saltBits == other._saltBits;
         }
 
         public bool Equals(string other)
@@ -145,13 +254,17 @@ namespace FsInfoCat.Util
             if (string.IsNullOrWhiteSpace(other))
                 return false;
             byte[] data;
-            try { data = Convert.FromBase64String(other); }
+            try
+            {
+                if (null == (data = Convert.FromBase64String(other)) || data.Length != TOTAL_BYTES_LENGTH)
+                    return false;
+            }
             catch { return false; }
-            return data.Length == TOTAL_LENGTH && BitConverter.ToUInt64(data, 0) == _hashBits0 &&
-                BitConverter.ToUInt64(data, FIELD_OFFSET_1) == _hashBits1 && BitConverter.ToUInt64(data, FIELD_OFFSET_2) == _hashBits2 &&
-                BitConverter.ToUInt64(data, FIELD_OFFSET_3) == _hashBits3 && BitConverter.ToUInt64(data, FIELD_OFFSET_4) == _hashBits4 &&
-                BitConverter.ToUInt64(data, FIELD_OFFSET_5) == _hashBits5 && BitConverter.ToUInt64(data, FIELD_OFFSET_6) == _hashBits6 &&
-                BitConverter.ToUInt64(data, FIELD_OFFSET_7) == _hashBits7 && BitConverter.ToUInt64(data, HASH_BYTES_LENGTH) == _saltBits;
+            return null != data && data.Length == TOTAL_BYTES_LENGTH && BitConverter.ToUInt64(data, 0) == _hashBits000_03f &&
+                BitConverter.ToUInt64(data, FIELD_OFFSET_1) == _hashBits040_07f && BitConverter.ToUInt64(data, FIELD_OFFSET_2) == _hashBits080_0bf &&
+                BitConverter.ToUInt64(data, FIELD_OFFSET_3) == _hashBits0c0_0ff && BitConverter.ToUInt64(data, FIELD_OFFSET_4) == _hashBits100_13f &&
+                BitConverter.ToUInt64(data, FIELD_OFFSET_5) == _hashBits140_17f && BitConverter.ToUInt64(data, FIELD_OFFSET_6) == _hashBits180_1bf &&
+                BitConverter.ToUInt64(data, FIELD_OFFSET_7) == _hashBits1c0_1ff && BitConverter.ToUInt64(data, HASH_BYTES_LENGTH) == _saltBits;
         }
 
         public override bool Equals(object obj)
@@ -167,14 +280,14 @@ namespace FsInfoCat.Util
         {
 #if CORE
             HashCode hashCode = new HashCode();
-            hashCode.Add(_hashBits0);
-            hashCode.Add(_hashBits1);
-            hashCode.Add(_hashBits2);
-            hashCode.Add(_hashBits3);
-            hashCode.Add(_hashBits4);
-            hashCode.Add(_hashBits5);
-            hashCode.Add(_hashBits6);
-            hashCode.Add(_hashBits7);
+            hashCode.Add(_hashBits000_03f);
+            hashCode.Add(_hashBits040_07f);
+            hashCode.Add(_hashBits080_0bf);
+            hashCode.Add(_hashBits0c0_0ff);
+            hashCode.Add(_hashBits100_13f);
+            hashCode.Add(_hashBits140_17f);
+            hashCode.Add(_hashBits180_1bf);
+            hashCode.Add(_hashBits1c0_1ff);
             hashCode.Add(_saltBits);
             return hashCode.ToHashCode();
 #else
