22aaed11badeb924f85847ded749f0abb6cd4dec
Leonard T. Erwine <lenny@erwinefamily.net>
2021-01-08 19:22:19 -0500

    Cleaned up formatting

--- src/FsInfoCat/Models/Crawl/MessageId.cs
+++ src/FsInfoCat/Models/Crawl/MessageId.cs
@@ -7,27 +7,41 @@ namespace FsInfoCat.Models.Crawl
     {
         [Description("No path was provied")]
         NoPathProvided,
+
         [Description("Error getting machine identifier.")]
         ErrorGettingMachineIdentifier,
+
         [Description("Path is invalid.")]
         InvalidPath,
+
         [Description("Path not found.")]
         PathNotFound,
+
         [Description("Unable to obtain name or length.")]
         FileSystemInfoPropertyAccessError,
+
         [Description("Unable to obtain creation time.")]
         CreationTimeAccessError,
+
         [Description("Unable to obtain last write time.")]
         LastWriteTimeAccessError,
+
         [Description("Unable to obtain file system attributes.")]
         AttributesAccessError,
+
         [Description("Unable to enmerate files.")]
         DirectoryFilesAccessError,
+
         [Description("Unable to enmerate subdirectories.")]
         SubdirectoriesAccessError,
+
         [Description("Crawl operation stopped.")]
         CrawlOperationStopped,
+
         [Description("Maximum crawl depth has been reached.")]
-        MaxDepthReached
+        MaxDepthReached,
+
+        [Description("An unexpected error has occurred.")]
+        UnexpectedError
     }
 }
