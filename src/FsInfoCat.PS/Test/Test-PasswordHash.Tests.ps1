355fb2d666992ec7375a6cc731c7c25a86526949
Leonard T. Erwine <lenny@erwinefamily.net>
2021-01-23 20:27:31 -0500

    Fixed syntax error

--- src/FsInfoCat.PS/Test/Test-PasswordHash.Tests.ps1
+++ src/FsInfoCat.PS/Test/Test-PasswordHash.Tests.ps1
@@ -6,47 +6,47 @@ Describe "Test-PasswordHash -RawPwd '<Password>' -EncodedPwHash '<EncodedPwHash>
     @{
         Password = 'FirstT3s!P@$S\/\/0rd';
         EncodedPwHash = 'QzY2tTqGtvLCBZuVNUAWlPGSTe9qJ4Vy7j3g4+M/osXqg01rr1ZLZZJHX3/esp7usc3dcb4efvYVqCRABPSardx4qT/hHCSP';
-        ExpectedResult = True;
+        ExpectedResult = $true;
     },
     @{
         Password = 'FirstT3s!P@$S\/\/0rd';
         EncodedPwHash = 'NkULLrCsaU3QbLpIvlF2twCdLUS3nKvVAIOV9upRHaWNlTIwZkE7tqIognuK1v3yqs37SHJIRKuyCf1nVhiZtJ3VHnJG6ZAp';
-        ExpectedResult = False;
+        ExpectedResult = $false;
     },
     @{
         Password = 'FirstT3s!P@$S\/\/0rd';
         EncodedPwHash = 'N7uuRU6dWa3z4O7DxslzRvTf/1n5HCnsfgK0fZXZ+t8oboxVP7kK57ycnMkHtQ69Rkx5jGWEAyh66/DdWINgaGOEmXn7EepX';
-        ExpectedResult = False;
+        ExpectedResult = $false;
     },
     @{
         Password = '1@3$5^7*9)';
         EncodedPwHash = 'QzY2tTqGtvLCBZuVNUAWlPGSTe9qJ4Vy7j3g4+M/osXqg01rr1ZLZZJHX3/esp7usc3dcb4efvYVqCRABPSardx4qT/hHCSP';
-        ExpectedResult = False;
+        ExpectedResult = $false;
     },
     @{
         Password = '1@3$5^7*9)';
         EncodedPwHash = 'NkULLrCsaU3QbLpIvlF2twCdLUS3nKvVAIOV9upRHaWNlTIwZkE7tqIognuK1v3yqs37SHJIRKuyCf1nVhiZtJ3VHnJG6ZAp';
-        ExpectedResult = True;
+        ExpectedResult = $false;
     },
     @{
         Password = '1@3$5^7*9)';
         EncodedPwHash = 'N7uuRU6dWa3z4O7DxslzRvTf/1n5HCnsfgK0fZXZ+t8oboxVP7kK57ycnMkHtQ69Rkx5jGWEAyh66/DdWINgaGOEmXn7EepX';
-        ExpectedResult = False;
+        ExpectedResult = $false;
     },
     @{
         Password = ' ';
         EncodedPwHash = 'QzY2tTqGtvLCBZuVNUAWlPGSTe9qJ4Vy7j3g4+M/osXqg01rr1ZLZZJHX3/esp7usc3dcb4efvYVqCRABPSardx4qT/hHCSP';
-        ExpectedResult = False;
+        ExpectedResult = $false;
     },
     @{
         Password = ' ';
         EncodedPwHash = 'NkULLrCsaU3QbLpIvlF2twCdLUS3nKvVAIOV9upRHaWNlTIwZkE7tqIognuK1v3yqs37SHJIRKuyCf1nVhiZtJ3VHnJG6ZAp';
-        ExpectedResult = False;
+        ExpectedResult = $false;
     },
     @{
         Password = ' ';
         EncodedPwHash = 'N7uuRU6dWa3z4O7DxslzRvTf/1n5HCnsfgK0fZXZ+t8oboxVP7kK57ycnMkHtQ69Rkx5jGWEAyh66/DdWINgaGOEmXn7EepX';
-        ExpectedResult = True;
+        ExpectedResult = $true;
     }
 ) {
     It "Returns <Base64String>"  {
