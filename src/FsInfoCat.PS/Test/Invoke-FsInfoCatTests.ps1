bb13bb1a8e2637a913a8beadfeb114d5b2fc7699
Leonard T. Erwine <lenny@erwinefamily.net>
2021-01-23 20:27:06 -0500

    Write results to log

--- src/FsInfoCat.PS/Test/Invoke-FsInfoCatTests.ps1
+++ src/FsInfoCat.PS/Test/Invoke-FsInfoCatTests.ps1
@@ -3,4 +3,5 @@
         -RecommendedAction "Execute the following command to install the module:`nInstall-Module -Name Pester" -ErrorAction Stop;
 }
 
-Invoke-Pester -Output Detailed;
+Set-Location -LiteralPath $PSScriptRoot;
+Invoke-Pester -OutputFile ($PSScriptRoot | Join-Path -ChildPath '../../../dev/log/FsInfoCat.Pester.log') -OutputFormat NUnitXml;
