25cc02b40bcbaca4ff4663e5bed80589553909f8
Leonard T. Erwine <lenny@erwinefamily.net>
2021-01-29 04:39:31 -0500

    Fixed inheritance warning

--- src/FsInfoCat/Models/Crawl/IFsChildNode.cs
+++ src/FsInfoCat/Models/Crawl/IFsChildNode.cs
@@ -4,7 +4,7 @@ namespace FsInfoCat.Models.Crawl
 {
     public interface IFsChildNode : IFsNode
     {
-        string Name { get; set; }
+        new string Name { get; set; }
         DateTime CreationTime { get; set; }
         DateTime LastWriteTime { get; set; }
         int Attributes { get; set; }
