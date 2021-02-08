63e0d5077cf978e8a66342529b5eb7e2957bc6ec
Leonard T. Erwine <lenny@erwinefamily.net>
2021-02-07 20:50:43 -0500

    Fixed assertion bugs

--- src/FsInfoCat.Test/VolumeIdentifierTest.cs
+++ src/FsInfoCat.Test/VolumeIdentifierTest.cs
@@ -49,6 +49,8 @@ namespace FsInfoCat.Test
         public IdValues FromGuidValueConstructorTest(Guid guid)
         {
             VolumeIdentifier target = new VolumeIdentifier(guid);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.False);
             Assert.That(target.Location.Scheme, Is.EqualTo("urn"));
@@ -65,6 +67,8 @@ namespace FsInfoCat.Test
         public UuidValues FromValidUUIDUriConstructorTest(string uri)
         {
             VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.False);
             Assert.That(target.Location.Scheme, Is.EqualTo("urn"));
@@ -77,6 +81,8 @@ namespace FsInfoCat.Test
         public IdValues FromSerialNumberValueConstructorTest(uint serialNumber)
         {
             VolumeIdentifier target = new VolumeIdentifier(serialNumber);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.False);
             Assert.That(target.Location.Scheme, Is.EqualTo("urn"));
@@ -93,14 +99,13 @@ namespace FsInfoCat.Test
         public SnIdValues FromSerialNumberAndOrdinalValuesConstructorTest(uint serialNumber, byte ordinal)
         {
             VolumeIdentifier target = new VolumeIdentifier(serialNumber, ordinal);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.False);
             Assert.That(target.Location.Scheme, Is.EqualTo("urn"));
             Assert.That(target.Location.Fragment, Is.Empty);
-            Assert.That(target.SerialNumber.HasValue, Is.True);
-            Assert.That(target.SerialNumber.Value, Is.EqualTo(serialNumber));
             Assert.That(target.UUID.HasValue, Is.False);
-            Assert.That(target.Ordinal.HasValue, Is.True);
             return new SnIdValues(target.SerialNumber, target.Ordinal, target.Location.AbsolutePath, target.Location.AbsoluteUri);
         }
 
@@ -109,11 +114,12 @@ namespace FsInfoCat.Test
         public SnIdValues FromValidSerialNumberAndOrdinalUriConstructorTest(string uri)
         {
             VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.False);
             Assert.That(target.Location.Scheme, Is.EqualTo("urn"));
             Assert.That(target.Location.Fragment, Is.Empty);
-            Assert.That(target.SerialNumber.HasValue, Is.True);
             Assert.That(target.UUID.HasValue, Is.False);
             return new SnIdValues(target.SerialNumber, target.Ordinal, target.Location.AbsolutePath, target.Location.AbsoluteUri);
         }
@@ -123,6 +129,8 @@ namespace FsInfoCat.Test
         public IdValues FromValidFilePathConstructorTest(string path)
         {
             VolumeIdentifier target = new VolumeIdentifier(path);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.True);
             Assert.That(target.Location.Scheme, Is.EqualTo("file"));
@@ -130,7 +138,7 @@ namespace FsInfoCat.Test
             Assert.That(target.SerialNumber.HasValue, Is.False);
             Assert.That(target.UUID.HasValue, Is.False);
             Assert.That(target.Ordinal.HasValue, Is.False);
-            return new IdValues(target.Location.AbsolutePath, target.Location.AbsoluteUri);
+            return new IdValues(target.Location.LocalPath, target.Location.AbsoluteUri);
         }
 
         [Test, Property("Priority", 3)]
@@ -138,6 +146,8 @@ namespace FsInfoCat.Test
         public IdValues FromValidFileUrlConstructorTest(string uri)
         {
             VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ValidationSucceeded));
+            Assert.That(target.IsValid(), Is.True);
             Assert.That(target.Location.IsAbsoluteUri, Is.True);
             Assert.That(target.Location.IsFile, Is.True);
             Assert.That(target.Location.Scheme, Is.EqualTo("file"));
@@ -145,35 +155,56 @@ namespace FsInfoCat.Test
             Assert.That(target.SerialNumber.HasValue, Is.False);
             Assert.That(target.UUID.HasValue, Is.False);
             Assert.That(target.Ordinal.HasValue, Is.False);
-            Assert.That(target.Location.AbsolutePath, Is.EqualTo(target.Location.LocalPath));
-            return new IdValues(target.Location.AbsolutePath, target.Location.AbsoluteUri);
+            return new IdValues(target.Location.LocalPath, target.Location.AbsoluteUri);
         }
 
         [Test, Property("Priority", 3)]
-        public void FromInvalidUriConstructorTest()
+        public void FromEmptyUriConstructorTest()
         {
-            Assert.That(() => { new VolumeIdentifier(null); }, Throws.ArgumentNullException
-                .With.Property("ParamName").EqualTo("url"));
-            Assert.That(() => { new VolumeIdentifier(_systemDrivePath); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("url"));
-            Assert.That(() => { new VolumeIdentifier(_systemDriveUrl); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("url"));
-            Assert.That(() => { new VolumeIdentifier("file:///"); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("url"));
+            string expected = "";
+            VolumeIdentifier target = new VolumeIdentifier(null);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.EmptyReferenceUrl));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.False);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(""));
+
+            target = new VolumeIdentifier(expected);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.EmptyReferenceUrl));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.False);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(expected));
         }
 
         [Test, Property("Priority", 3)]
-        public void FromInvalidUriFormatConstructorTest([Values("", " ", ".", "urn")] string uri)
+        public void FromRelativeUriFormatConstructorTest([Values(" ", ".", "urn", "test/file")] string uri)
         {
-            Assert.That(() => { new VolumeIdentifier(uri); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("url"));
+            VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.ReferenceUrlNotAbsolute));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.False);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(uri));
         }
 
         [Test, Property("Priority", 3)]
         public void FromInvalidUrnNamespaceConstructorTest([Values("urn:", "urn:id")] string uri)
         {
-            Assert.That(() => { new VolumeIdentifier(uri); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("uri"));
+            VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.InvalidUrnNamespace));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.True);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(uri));
         }
 
         [Test, Property("Priority", 3)]
@@ -181,8 +212,14 @@ namespace FsInfoCat.Test
             "urn:uuid", "urn:uuid:", "urn:uuid:fb4f6ff1-7ffe-4eef-840a-1df28bb2e178:2", "urn:uuid:fb4f6ff1-7ffe-4eef-840a-1df28bb2e17"
         )] string uri)
         {
-            Assert.That(() => { new VolumeIdentifier(uri); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("uri"));
+            VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.InvalidUUID));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.True);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(uri));
         }
 
         [Test, Property("Priority", 3)]
@@ -190,8 +227,14 @@ namespace FsInfoCat.Test
             "urn:volume", "urn:volume:", "urn:volume:id", "urn:volume:id:", "urn:volume:id:3B51-8D4B-1", "urn:volume:id:3B518D4B-"
         )] string uri)
         {
-            Assert.That(() => { new VolumeIdentifier(uri); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("uri"));
+            VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.InvalidVolumeID));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.True);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(uri));
         }
 
         [Test, Property("Priority", 3)]
@@ -199,8 +242,14 @@ namespace FsInfoCat.Test
             "http://tempuri.org", "mailto:volume@id.3B51-8D4B"
         )] string uri)
         {
-            Assert.That(() => { new VolumeIdentifier(uri); }, Throws.InstanceOf<ArgumentOutOfRangeException>()
-                .With.Property("ParamName").EqualTo("url"));
+            VolumeIdentifier target = new VolumeIdentifier(uri);
+            Assert.That(target.GetValidationCode(), Is.EqualTo(VolumeIdentifier.ValidationCode.InvalidUriScheme));
+            Assert.That(target.IsValid(), Is.False);
+            Assert.That(target.SerialNumber.HasValue, Is.False);
+            Assert.That(target.UUID.HasValue, Is.False);
+            Assert.That(target.Ordinal.HasValue, Is.False);
+            Assert.That(target.Location.IsAbsoluteUri, Is.True);
+            Assert.That(target.Location.OriginalString, Is.EqualTo(uri));
         }
     }
 }
