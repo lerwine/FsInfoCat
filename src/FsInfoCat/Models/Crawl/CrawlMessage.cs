b9b8dccfc1858380248e92a540b794c3b699f01b
Leonard T. Erwine <lenny@erwinefamily.net>
2021-03-24 00:42:34 -0400

    Remived IEquatable from CrawlMessage

--- src/FsInfoCat/Models/Crawl/CrawlMessage.cs
+++ src/FsInfoCat/Models/Crawl/CrawlMessage.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace FsInfoCat.Models.Crawl
 {
-    public abstract class CrawlMessage : ComponentBase, IEquatable<CrawlMessage>
+    public abstract class CrawlMessage : ComponentBase
     {
         private string _message = "";
         public string Message
@@ -31,15 +31,5 @@ namespace FsInfoCat.Models.Crawl
         }
 
         public CrawlMessage() { }
-
-        public bool Equals(CrawlMessage other)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool Equals([Microsoft.CodeAnalysis.Host] object obj)
-        {
-            return Equals(obj as CrawlMessage);
-        }
     }
 }
