12450845224750f92fbd4d4fb685224b9e6ad6ed
Leonard T. Erwine <lenny@erwinefamily.net>
2021-04-13 17:44:23 -0400

    Added database project

---
+++ src/FsInfoCatDb/dbo/Tables/Account.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Account] (
+    [AccountID]   UNIQUEIDENTIFIER NOT NULL,
+    [DisplayName] NVARCHAR (128)   DEFAULT ('') NOT NULL,
+    [LoginName]   NVARCHAR (32)    NOT NULL,
+    [Role]        TINYINT          NOT NULL,
+    [Notes]       NTEXT            DEFAULT ('') NOT NULL,
+    [CreatedOn]   DATETIME         NOT NULL,
+    [CreatedBy]   UNIQUEIDENTIFIER NOT NULL,
+    [ModifiedOn]  DATETIME         NOT NULL,
+    [ModifiedBy]  UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([AccountID] ASC),
+    CONSTRAINT [FK_Account_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Account] ([AccountID]),
+    CONSTRAINT [FK_Account_ModifiedBy] FOREIGN KEY ([ModifiedBy]) REFERENCES [dbo].[Account] ([AccountID])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IDX_Account_LoginName]
+    ON [dbo].[Account]([LoginName] ASC);
+
---
+++ src/FsInfoCatDb/dbo/Tables/HostDevice.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[HostDevice] (
+    [HostDeviceID]     UNIQUEIDENTIFIER NOT NULL,
+    [DisplayName]      NVARCHAR (128)   DEFAULT ('') NOT NULL,
+    [MachineIdentifer] NVARCHAR (128)   NOT NULL,
+    [MachineName]      NVARCHAR (128)   NOT NULL,
+    [Platform]         TINYINT          DEFAULT ((0)) NOT NULL,
+    [AllowCrawl]       BIT              DEFAULT ((0)) NOT NULL,
+    [IsInactive]       BIT              DEFAULT ((0)) NOT NULL,
+    [Notes]            NTEXT            DEFAULT ('') NOT NULL,
+    [CreatedOn]        DATETIME         NOT NULL,
+    [CreatedBy]        UNIQUEIDENTIFIER NOT NULL,
+    [ModifiedOn]       DATETIME         NOT NULL,
+    [ModifiedBy]       UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_HostDevice] PRIMARY KEY CLUSTERED ([HostDeviceID] ASC),
+    CONSTRAINT [FK_HostDevice_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Account] ([AccountID]),
+    CONSTRAINT [FK_HostDevice_ModifiedBy] FOREIGN KEY ([ModifiedBy]) REFERENCES [dbo].[Account] ([AccountID]),
+    CONSTRAINT [AK_HostDevice_MachineIdentifer] UNIQUE NONCLUSTERED ([MachineIdentifer] ASC),
+    CONSTRAINT [AK_HostDevice_MachineIdentifier] UNIQUE NONCLUSTERED ([DisplayName] ASC),
+    CONSTRAINT [AK_HostDevice_MachineName] UNIQUE NONCLUSTERED ([MachineName] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IDX_HostDevice_MachineIdentifer]
+    ON [dbo].[HostDevice]([MachineIdentifer] ASC);
+
