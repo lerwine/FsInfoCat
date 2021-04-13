588f27f58c6ba741b9f3d952f195cf1e9e45f678
Leonard T. Erwine <lenny@erwinefamily.net>
2021-04-13 18:01:27 -0400

    Added data model

--- src/FsInfoCat.Desktop/Properties/Settings.Designer.cs
+++ src/FsInfoCat.Desktop/Properties/Settings.Designer.cs
@@ -8,21 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace FsInfoCat.Desktop.Properties
-{
+namespace FsInfoCat.Desktop.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=c868dbserver.database.windows.net;Initial Catalog=FsInfoCat;Persist S" +
+            "ecurity Info=True;User ID=fsinfocatadmin;Connect Timeout=30;Encrypt=True")]
+        public string FsInfoCatConnectionString {
+            get {
+                return ((string)(this["FsInfoCatConnectionString"]));
             }
         }
     }
