fc2c1f9777cdd98e4a035f41724f50534b354433
Leonard T. Erwine <lenny@erwinefamily.net>
2021-04-12 22:31:58 -0400

    Removed obsolete files and projects and added desktop app

--- src/FsInfoCat.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FsInfoCat.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- src/FsInfoCat.Web/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-var UserRole;
-(function (UserRole) {
-    UserRole[UserRole["None"] = 0] = "None";
-    UserRole[UserRole["Viewer"] = 1] = "Viewer";
-    UserRole[UserRole["User"] = 2] = "User";
-    UserRole[UserRole["Crawler"] = 3] = "Crawler";
-    UserRole[UserRole["Admin"] = 4] = "Admin";
-})(UserRole || (UserRole = {}));
