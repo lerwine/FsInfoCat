c37aff8860b71af35f9720a831487e93bd4568c8
Leonard T. Erwine <lenny@erwinefamily.net>
2021-04-13 21:38:22 -0400

    Removed DB connection string edit window. Too much work for now.

--- src/FsInfoCat.Desktop/ViewModel/SettingsViewModel.cs
+++ src/FsInfoCat.Desktop/ViewModel/SettingsViewModel.cs
@@ -10,30 +10,5 @@ namespace FsInfoCat.Desktop.ViewModel
 {
     public class SettingsViewModel : DependencyObject
     {
-        public event DependencyPropertyChangedEventHandler DbConnectionSettingsPropertyChanged;
-
-        public static readonly DependencyProperty DbConnectionSettingsProperty =
-            DependencyProperty.Register(nameof(DbConnectionSettings), typeof(DbConnectionSettingsViewModel), typeof(SettingsViewModel),
-                new PropertyMetadata(null,
-                    (DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as SettingsViewModel).OnDbConnectionSettingsPropertyChanged(e),
-                    (DependencyObject d, object baseValue) => baseValue ?? new DbConnectionSettingsViewModel()));
-
-        public DbConnectionSettingsViewModel DbConnectionSettings
-        {
-            get { return (DbConnectionSettingsViewModel)GetValue(DbConnectionSettingsProperty); }
-            set { SetValue(DbConnectionSettingsProperty, value); }
-        }
-
-        protected virtual void OnDbConnectionSettingsPropertyChanged(DependencyPropertyChangedEventArgs args)
-        {
-            try { OnDbConnectionSettingsPropertyChanged((DbConnectionSettingsViewModel)args.OldValue, (DbConnectionSettingsViewModel)args.NewValue); }
-            finally { DbConnectionSettingsPropertyChanged?.Invoke(this, args); }
-        }
-
-        protected virtual void OnDbConnectionSettingsPropertyChanged(DbConnectionSettingsViewModel oldValue, DbConnectionSettingsViewModel newValue)
-        {
-            // TODO: Implement OnDbConnectionSettingsPropertyChanged Logic
-        }
-
     }
 }
