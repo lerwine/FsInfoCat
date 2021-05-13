330b2fd655db685cb223512a75c167ec4b426b17
Leonard T. Erwine <lenny@erwinefamily.net>
2021-05-13 01:04:20 -0400

    Code cleanup

--- src/FsInfoCat/Collections/GeneralizableObservableLinkedListBase.cs
+++ src/FsInfoCat/Collections/GeneralizableObservableLinkedListBase.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Text;
-using System.Threading;
 using System.Linq;
+using System.Threading;
 
 namespace FsInfoCat.Collections
 {
