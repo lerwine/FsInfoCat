a12481e22bc4721880d97d3ac90bd4c33ee9ca11
Leonard T. Erwine <lenny@erwinefamily.net>
2021-06-13 00:49:52 -0400

    Added converters and wpf resources

---
+++ FsInfoCat/FsInfoCat.Desktop/Converters/AccountToStringConverter.cs
@@ -0,0 +1,22 @@
+using FsInfoCat.Upstream;
+using System.Globalization;
+using System.Windows;
+
+namespace FsInfoCat.Desktop.Converters
+{
+    public class AccountToStringConverter : ToClassConverterBase<IUserProfile, string>
+    {
+
+        public static readonly DependencyProperty NullSourceProperty =
+            DependencyProperty.Register(nameof(NullSource), typeof(string), typeof(AccountToStringConverter),
+                new PropertyMetadata(""));
+
+        public override string NullSource
+        {
+            get { return GetValue(NullSourceProperty) as string; }
+            set { SetValue(NullSourceProperty, value); }
+        }
+
+        public override string Convert(IUserProfile value, object parameter, CultureInfo culture) => throw new System.NotImplementedException();//value.DisplayName
+    }
+}
