3be6e71fa94954f2791d1c98d032cfda05d86764
Leonard T. Erwine <lenny@erwinefamily.net>
2021-06-18 23:16:52 -0400

    Added load and save methods.

--- FsInfoCat/FsInfoCat.PS/Export/ExportSet.cs
+++ FsInfoCat/FsInfoCat.PS/Export/ExportSet.cs
@@ -1,9 +1,12 @@
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace FsInfoCat.PS.Export
 {
-    public class ExportSet
+    public sealed class ExportSet
     {
         [XmlElement(nameof(FileSystem))]
         public Collection<FileSystem> FileSystems { get; set; }
@@ -13,6 +16,76 @@ namespace FsInfoCat.PS.Export
 
         [XmlElement(nameof(ContentInfo))]
         public Collection<ContentInfo> ContentInfos { get; set; }
+
+        private static XmlWriterSettings GetDefaultXmlWriterSettings() => new()
+        {
+            Indent = true,
+            Encoding = new UTF8Encoding(false, true)
+        };
+
+        public static ExportSet Load(string path)
+        {
+            using XmlReader xmlReader = XmlReader.Create(path);
+            return Load(xmlReader);
+        }
+
+        public static ExportSet Load(TextReader input)
+        {
+            using XmlReader xmlReader = XmlReader.Create(input);
+            return Load(xmlReader);
+        }
+
+        public static ExportSet LoadXml(string xml)
+        {
+            using StringReader reader = new(xml);
+            return Load(reader);
+        }
+
+        public static ExportSet Load(Stream input)
+        {
+            using XmlReader xmlReader = XmlReader.Create(input);
+            return Load(xmlReader);
+        }
+
+        public static ExportSet Load(XmlReader xmlReader)
+        {
+            XmlSerializer serializer = new(typeof(ExportSet));
+            return (ExportSet)serializer.Deserialize(xmlReader);
+        }
+
+        public void WriteTo(string path, XmlWriterSettings settings = null)
+        {
+            using XmlWriter xmlWriter = XmlWriter.Create(path, settings ?? GetDefaultXmlWriterSettings());
+            WriteTo(xmlWriter);
+            xmlWriter.Flush();
+        }
+
+        public void WriteTo(Stream output, XmlWriterSettings settings = null)
+        {
+            using XmlWriter xmlWriter = XmlWriter.Create(output, settings ?? GetDefaultXmlWriterSettings());
+            WriteTo(xmlWriter);
+            xmlWriter.Flush();
+        }
+
+        public void WriteTo(TextWriter output, XmlWriterSettings settings = null)
+        {
+            using XmlWriter xmlWriter = XmlWriter.Create(output, settings ?? GetDefaultXmlWriterSettings());
+            WriteTo(xmlWriter);
+            xmlWriter.Flush();
+        }
+
+        public string ToXmlString(XmlWriterSettings settings = null)
+        {
+            StringWriter writer = new();
+            WriteTo(writer, settings);
+            return writer.ToString();
+        }
+
+        public void WriteTo(XmlWriter xmlWriter)
+        {
+            XmlSerializer serializer = new(typeof(ExportSet));
+            serializer.Serialize(xmlWriter, this);
+        }
 
         public abstract class FileSystemBase : EntityExportElement
         {
