be7ddc3e52b6567a67b5daa6b65572975c96fcdd
Leonard T. Erwine <lenny@erwinefamily.net>
2021-09-22 17:12:30 -0400

    Added polymorphism diagram

--- src/FsInfoCat/IDbFsItemAncestorName.cs
+++ src/FsInfoCat/IDbFsItemAncestorName.cs
@@ -1,9 +1,22 @@
-﻿namespace FsInfoCat
+namespace FsInfoCat
 {
+    /// <summary>
+    /// Interface for objects that contain the name of a file system node and a delimited list of parent subdirectory names.
+    /// Implements the <see cref="FsInfoCat.IHasSimpleIdentifier" />
+    /// </summary>
+    /// <seealso cref="FsInfoCat.IHasSimpleIdentifier" />
     public interface IDbFsItemAncestorName : IHasSimpleIdentifier
     {
-        string Name { get; }
+        /// <summary>
+        /// Gets the name of teh current file system node.
+        /// </summary>
+        /// <value>The name current file system node (file or directory).</value>
+         string Name { get; }
 
+        /// <summary>
+        /// Gets the ancestor subdirectory names.
+        /// </summary>
+        /// <value>The result of a calculated column that contains the names of the parent subdirectories, separated by a slash (<>c</c>) character, and in reverse order from typical file system path segments.</value>
         string AncestorNames { get; }
     }
 }
