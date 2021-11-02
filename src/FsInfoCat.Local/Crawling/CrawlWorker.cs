207cb56d1d261fb1729563f749b4130b48a482da
erwinel <leonard.t.erwine.ctr@mail.mil>
2021-11-02 16:09:01 -0400

    Resolves #104

--- src/FsInfoCat.Local/Crawling/CrawlWorker.cs
+++ src/FsInfoCat.Local/Crawling/CrawlWorker.cs
@@ -1,91 +1,70 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace FsInfoCat.Local.Crawling
 {
-    class CrawlWorker
+    partial class CrawlWorker
     {
         private readonly ILogger<CrawlWorker> _logger;
         private readonly ICrawlMessageBus _crawlMessageBus;
         private readonly IFileSystemDetailService _fileSystemDetailService;
+        private readonly ILocalCrawlConfiguration _crawlConfiguration;
 
-        public DateTime? StopAt { get; }
+        public DateTime? StopAt { get; private set; }
 
-        public long? TTL { get; }
+        public TimeSpan? TTL { get; }
 
         public ICurrentItem CurrentItem { get; internal set; }
 
+        public long ItemsCrawled { get; internal set; }
+
         internal CrawlWorker([DisallowNull] ILocalCrawlConfiguration crawlConfiguration, [DisallowNull] ICrawlMessageBus crawlMessageBus, [DisallowNull] IFileSystemDetailService fileSystemDetailService, DateTime? stopAt)
         {
-            (_logger, _crawlMessageBus, _fileSystemDetailService) = (Hosting.GetRequiredService<ILogger<CrawlWorker>>(), crawlMessageBus, fileSystemDetailService);
+            long? ttl = crawlConfiguration.TTL;
+            TTL = ttl.HasValue ? TimeSpan.FromSeconds(ttl.Value) : null;
+            (_logger, _crawlMessageBus, _fileSystemDetailService, _crawlConfiguration, StopAt) = (Hosting.GetRequiredService<ILogger<CrawlWorker>>(), crawlMessageBus, fileSystemDetailService, crawlConfiguration, stopAt);
             _logger.LogDebug($"{nameof(CrawlWorker)} instantiated");
         }
 
-        // TODO: Implement CrawlWorker.DoWorkAsync #104
         internal async Task<bool?> DoWorkAsync(CancellationToken cancellationToken)
         {
+            long? ttl = _crawlConfiguration.TTL;
+            if (ttl.HasValue)
+            {
+                DateTime stopAt = DateTime.Now.Add(TimeSpan.FromSeconds(ttl.Value));
+                if (!(StopAt.HasValue && StopAt.Value.CompareTo(stopAt) < 0))
+                    StopAt = stopAt;
+            }
+            if (StopAt.HasValue && StopAt.Value.CompareTo(DateTime.Now) <= 0)
+                return false;
+            using IServiceScope serviceScope = Hosting.CreateScope();
+            using LocalDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<LocalDbContext>();
+            Subdirectory subdirectory = await dbContext.Subdirectories.FindAsync(_crawlConfiguration.RootId, cancellationToken);
+            if (StopAt.HasValue && StopAt.Value.CompareTo(DateTime.Now) <= 0)
+                return false;
+            if (subdirectory is null)
+                throw new InvalidOperationException("Could not find subdirectory record for crawl configuration");
+            return await CrawlAsync(new CurrentDirectory()
+            {
+                Target = await subdirectory.GetDirectoryInfoAsync(_fileSystemDetailService, dbContext, cancellationToken),
+                Entity = subdirectory
+            }, cancellationToken);
+        }
+
+        // Return true if item limit reached; false if timeout, null if completed
+        private async Task<bool?> CrawlAsync(CurrentDirectory currentDirectory, CancellationToken cancellationToken)
+        {
+            CurrentItem = currentDirectory;
+            if (StopAt.HasValue && StopAt.Value.CompareTo(DateTime.Now) <= 0)
+                return false;
+            // TODO: Implement CrawlWorker.CrawlAsync
             throw new NotImplementedException();
-        }
-
-        record CurrentFile : ICurrentItem
-        {
-            internal FileInfo Target { get; init; }
-
-            internal DbFile Entity { get; init; }
-
-            internal CurrentDirectory Parent { get; init; }
-
-            string ICurrentItem.Name => Target.Name ?? Entity.Name;
-
-            FileSystemInfo ICurrentItem.Target => Target;
-
-            ILocalDbFsItem ICurrentItem.Entity => Entity;
-
-            string ICurrentItem.GetFullName() => Target?.FullName ?? Path.Combine(Parent.GetFullName(), Target.Name ?? Entity.Name);
-
-            string ICurrentItem.GetRelativeParentPath()
-            {
-                string path = Parent.GetRelativeParentPath();
-                return string.IsNullOrEmpty(path) ? Target.Name ?? Entity.Name : Path.Combine(path, Target.Name ?? Entity.Name);
-            }
-        }
-
-        record CurrentDirectory : ICurrentItem
-        {
-            internal DirectoryInfo Target { get; init; }
-
-            internal Subdirectory Entity { get; init; }
-
-            internal CurrentDirectory Parent { get; init; }
-
-            string ICurrentItem.Name => Target.Name ?? Entity.Name;
-
-            FileSystemInfo ICurrentItem.Target => Target;
-
-            ILocalDbFsItem ICurrentItem.Entity => Entity;
-
-            public string GetFullName()
-            {
-                if (Target is null)
-                {
-                    if (Parent is null)
-                        return Entity.Name;
-                    return Path.Combine(Parent.GetFullName(), Entity.Name);
-                }
-                return Target.FullName;
-            }
-
-            public string GetRelativeParentPath()
-            {
-                if (Parent is null)
-                    return "";
-                string path = Parent.GetRelativeParentPath();
-                return (path.Length > 0) ? Path.Combine(path, Target.Name ?? Entity.Name) : Target.Name ?? Entity.Name;
-            }
         }
     }
 }
