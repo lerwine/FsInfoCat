70d733648b86049cd57474b641b5a88a06d4d0e3
Leonard T. Erwine <lenny@erwinefamily.net>
2021-11-05 01:56:55 -0400

    Resolves #119

--- src/FsInfoCat/Collections/MultiStringValue.CharValueBuilder.cs
+++ src/FsInfoCat/Collections/MultiStringValue.CharValueBuilder.cs
@@ -8,8 +8,8 @@ namespace FsInfoCat.Collections
         {
             private readonly char _value;
             internal CharValueBuilder(char c) { _value = c; }
-            public IValueBuilder Append(string text) => string.IsNullOrEmpty(text) ? this : new AppendedValueBuilder(new StringBuilder(_value).Append(text));
-            public IValueBuilder Append(char c) => new AppendedValueBuilder(new StringBuilder(_value).Append(c));
+            public IValueBuilder Append(string text) => string.IsNullOrEmpty(text) ? this : new AppendedValueBuilder(new StringBuilder().Append(_value).Append(text));
+            public IValueBuilder Append(char c) => new AppendedValueBuilder(new StringBuilder().Append(_value).Append(c));
             public bool TryGetValue(out string text) { text = GetValue(); return true; }
             public string GetValue() => new(new char[] { _value });
         }
