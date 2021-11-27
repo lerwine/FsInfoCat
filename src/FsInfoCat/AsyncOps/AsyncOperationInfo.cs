20fd68b93ee97e2d2a1816e8a35f135a6037d5e2
Leonard T. Erwine <lenny@erwinefamily.net>
2021-11-27 17:52:16 -0500

    Removed unused types

--- src/FsInfoCat/AsyncOps/AsyncOperationInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace FsInfoCat.AsyncOps
-{
-    [Obsolete("Use FsInfoCat.AsyncOps.IBackgroundProgressInfo, instead")]
-    public record AsyncOperationInfo(Guid ConcurrencyId, AsyncJobStatus Status, ActivityCode? Activity, MessageCode? StatusDescription, string CurrentOperation, object AsyncState, IAsyncOperationInfo ParentOperation = null) : IAsyncOperationInfo;
-}
--- src/FsInfoCat/OperationStatus.cs
+++
@@ -1,5 +0,0 @@
-namespace FsInfoCat
-{
-    [System.Obsolete("Use FsInfoCat.AsyncOps.IBackgroundProgressInfo, instead")]
-    public record OperationStatus(MessageCode StatusDescription, string CurrentOperation);
-}
