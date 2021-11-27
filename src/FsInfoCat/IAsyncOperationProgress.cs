8625a7d07c770157a386a34f6ef19b9192a7ed77
Leonard T. Erwine <lenny@erwinefamily.net>
2021-11-27 18:22:13 -0500

    Updated obsolete message

--- src/FsInfoCat/IAsyncOperationProgress.cs
+++ src/FsInfoCat/IAsyncOperationProgress.cs
@@ -3,7 +3,7 @@ using System.Threading;
 
 namespace FsInfoCat
 {
-    [Obsolete("Use FsInfoCat.AsyncOps.IBackgroundProgressInfo, instead")]
+    [Obsolete("Use System.IProgress<FsInfoCat.AsyncOps.IBackgroundProgressInfo>, instead")]
     public interface IAsyncOperationProgress : IProgress<IAsyncOperationInfo>
     {
         CancellationToken Token { get; }
