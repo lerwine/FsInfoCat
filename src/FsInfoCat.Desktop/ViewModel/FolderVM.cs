a8d7483e227b69af964fcc0269173d7ee6d43acd
Leonard T. Erwine <lenny@erwinefamily.net>
2022-02-06 15:48:39 -0500

    Code cleanup

--- src/FsInfoCat.Desktop/ViewModel/FolderVM.cs
+++ src/FsInfoCat.Desktop/ViewModel/FolderVM.cs
@@ -16,17 +16,23 @@ namespace FsInfoCat.Desktop.ViewModel
     {
         private readonly object _syncRoot = new();
         private CancellableTask<Task, int> _preload;
+        private readonly ILogger<FolderVM> _logger;
+
+        #region Name Dependency Property Members
 
         private static readonly DependencyPropertyKey NamePropertyKey = DependencyProperty.RegisterReadOnly(nameof(Name), typeof(string), typeof(FolderVM), new PropertyMetadata(""));
 
         public static readonly DependencyProperty NameProperty = NamePropertyKey.DependencyProperty;
-        private readonly ILogger<FolderVM> _logger;
 
         public string Name
         {
             get => GetValue(NameProperty) as string;
             private set => SetValue(NamePropertyKey, value);
         }
+
+        #endregion
+
+        #region Path Dependency Property Members
 
         private static readonly DependencyPropertyKey PathPropertyKey = DependencyProperty.RegisterReadOnly(nameof(Path), typeof(string), typeof(FolderVM), new PropertyMetadata(""));
 
@@ -38,7 +44,163 @@ namespace FsInfoCat.Desktop.ViewModel
             private set => SetValue(PathPropertyKey, value);
         }
 
+        #endregion
+
+        #region Attributes Dependency Property Members
+
         private static readonly DependencyPropertyKey AttributesPropertyKey = DependencyProperty.RegisterReadOnly(nameof(Attributes), typeof(FileAttributes), typeof(FolderVM), new PropertyMetadata(FileAttributes.Normal));
+
+        public static readonly DependencyProperty AttributesProperty = AttributesPropertyKey.DependencyProperty;
+
+        public FileAttributes Attributes
+        {
+            get => (FileAttributes)GetValue(AttributesProperty);
+            private set => SetValue(AttributesPropertyKey, value);
+        }
+
+        #endregion
+
+        #region CreationTime Dependency Property Members
+
+        private static readonly DependencyPropertyKey CreationTimePropertyKey = DependencyProperty.RegisterReadOnly(nameof(CreationTime), typeof(DateTime), typeof(FolderVM), new PropertyMetadata(DateTime.Now));
+
+        public static readonly DependencyProperty CreationTimeProperty = CreationTimePropertyKey.DependencyProperty;
+
+        public DateTime CreationTime
+        {
+            get => (DateTime)GetValue(CreationTimeProperty);
+            private set => SetValue(CreationTimePropertyKey, value);
+        }
+
+        #endregion
+
+        #region LastWriteTime Dependency Property Members
+
+        private static readonly DependencyPropertyKey LastWriteTimePropertyKey = DependencyProperty.RegisterReadOnly(nameof(LastWriteTime), typeof(DateTime), typeof(FolderVM), new PropertyMetadata(DateTime.Now));
+
+        public static readonly DependencyProperty LastWriteTimeProperty = LastWriteTimePropertyKey.DependencyProperty;
+
+        public DateTime LastWriteTime
+        {
+            get => (DateTime)GetValue(LastWriteTimeProperty);
+            private set => SetValue(LastWriteTimePropertyKey, value);
+        }
+
+        #endregion
+
+        #region IsSelected Dependency Property Members
+
+        public static readonly DependencyProperty IsSelectedProperty = DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(FolderVM),
+                new PropertyMetadata(false, (DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as FolderVM).OnIsSelectedPropertyChanged(e)));
+
+        public bool IsSelected
+        {
+            get => (bool)GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+
+        protected virtual void OnIsSelectedPropertyChanged(DependencyPropertyChangedEventArgs args)
+        {
+            if ((bool)args.NewValue)
+                _ = PreloadAsync();
+        }
+
+        #endregion
+
+        #region IsExpanded Dependency Property Members
+
+        public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(nameof(IsExpanded), typeof(bool), typeof(FolderVM),
+                new PropertyMetadata(false, (DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as FolderVM).OnIsExpandedPropertyChanged(e)));
+
+        public bool IsExpanded
+        {
+            get => (bool)GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
+        }
+
+        protected virtual void OnIsExpandedPropertyChanged(DependencyPropertyChangedEventArgs args)
+        {
+            if ((bool)args.NewValue)
+                _ = PreloadAsync();
+        }
+
+        #endregion
+
+        #region SubFolders Dependency Property Members
+
+        private static readonly DependencyPropertyKey InnerSubFoldersPropertyKey = DependencyProperty.RegisterReadOnly(nameof(InnerSubFolders), typeof(ObservableCollection<FolderVM>), typeof(FolderVM), new PropertyMetadata(null));
+
+        private static readonly DependencyPropertyKey SubFoldersPropertyKey = DependencyProperty.RegisterReadOnly(nameof(SubFolders), typeof(ReadOnlyObservableCollection<FolderVM>), typeof(FolderVM), new PropertyMetadata(null));
+
+        protected static readonly DependencyProperty InnerSubFoldersProperty = InnerSubFoldersPropertyKey.DependencyProperty;
+
+        public static readonly DependencyProperty SubFoldersProperty = SubFoldersPropertyKey.DependencyProperty;
+
+        protected ObservableCollection<FolderVM> InnerSubFolders
+        {
+            get => (ObservableCollection<FolderVM>)GetValue(InnerSubFoldersProperty);
+            private set => SetValue(InnerSubFoldersPropertyKey, value);
+        }
+
+        public ReadOnlyObservableCollection<FolderVM> SubFolders
+        {
+            get => (ReadOnlyObservableCollection<FolderVM>)GetValue(SubFoldersProperty);
+            private set => SetValue(SubFoldersPropertyKey, value);
+        }
+
+        #endregion
+
+        #region Dependency Property Property Members
+
+        private static readonly DependencyPropertyKey InnerFilesPropertyKey = DependencyProperty.RegisterReadOnly(nameof(InnerFiles), typeof(ObservableCollection<FileVM>), typeof(FolderVM), new PropertyMetadata(null));
+
+        private static readonly DependencyPropertyKey FilesPropertyKey = DependencyProperty.RegisterReadOnly(nameof(Files), typeof(ReadOnlyObservableCollection<FileVM>), typeof(FolderVM), new PropertyMetadata(null));
+
+        protected static readonly DependencyProperty InnerFilesProperty = InnerFilesPropertyKey.DependencyProperty;
+
+        public static readonly DependencyProperty FilesProperty = FilesPropertyKey.DependencyProperty;
+
+        protected ObservableCollection<FileVM> InnerFiles
+        {
+            get => (ObservableCollection<FileVM>)GetValue(InnerFilesProperty);
+            private set => SetValue(InnerFilesPropertyKey, value);
+        }
+
+        public ReadOnlyObservableCollection<FileVM> Files
+        {
+            get => (ReadOnlyObservableCollection<FileVM>)GetValue(FilesProperty);
+            private set => SetValue(FilesPropertyKey, value);
+        }
+
+        #endregion
+
+        #region AccessError Dependency Property Members
+
+        private static readonly DependencyPropertyKey AccessErrorPropertyKey = DependencyProperty.RegisterReadOnly(nameof(AccessError), typeof(string), typeof(FolderVM), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty AccessErrorProperty = AccessErrorPropertyKey.DependencyProperty;
+
+        public string AccessError
+        {
+            get => GetValue(AccessErrorProperty) as string;
+            private set => SetValue(AccessErrorPropertyKey, value);
+        }
+
+        #endregion
+
+        private FolderVM(string name, DirectoryInfo directoryInfo)
+        {
+            _logger = App.GetLogger(this);
+            Name = name;
+            Path = directoryInfo.FullName;
+            Attributes = directoryInfo.Attributes;
+            CreationTime = directoryInfo.CreationTime;
+            LastWriteTime = directoryInfo.LastWriteTime;
+            InnerSubFolders = new();
+            SubFolders = new(InnerSubFolders);
+            InnerFiles = new();
+            Files = new(InnerFiles);
+        }
 
         internal static Task<LinkedList<FolderVM>> FindByPathAsync(IEnumerable<FolderVM> logicalDisks, string path) => Task.Factory.StartNew(() => FindByPath(logicalDisks, path));
 
@@ -93,136 +255,6 @@ namespace FsInfoCat.Desktop.ViewModel
                 _ = list.AddLast(item);
             }
             return list;
-        }
-
-        public static readonly DependencyProperty AttributesProperty = AttributesPropertyKey.DependencyProperty;
-
-        public FileAttributes Attributes
-        {
-            get => (FileAttributes)GetValue(AttributesProperty);
-            private set => SetValue(AttributesPropertyKey, value);
-        }
-
-        private static readonly DependencyPropertyKey CreationTimePropertyKey = DependencyProperty.RegisterReadOnly(nameof(CreationTime), typeof(DateTime), typeof(FolderVM), new PropertyMetadata(DateTime.Now));
-
-        public static readonly DependencyProperty CreationTimeProperty = CreationTimePropertyKey.DependencyProperty;
-
-        public DateTime CreationTime
-        {
-            get => (DateTime)GetValue(CreationTimeProperty);
-            private set => SetValue(CreationTimePropertyKey, value);
-        }
-
-        private static readonly DependencyPropertyKey LastWriteTimePropertyKey = DependencyProperty.RegisterReadOnly(nameof(LastWriteTime), typeof(DateTime), typeof(FolderVM), new PropertyMetadata(DateTime.Now));
-
-        public static readonly DependencyProperty LastWriteTimeProperty = LastWriteTimePropertyKey.DependencyProperty;
-
-        public DateTime LastWriteTime
-        {
-            get => (DateTime)GetValue(LastWriteTimeProperty);
-            private set => SetValue(LastWriteTimePropertyKey, value);
-        }
-
-        public static readonly DependencyProperty IsSelectedProperty = DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(FolderVM),
-                new PropertyMetadata(false, (DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as FolderVM).OnIsSelectedPropertyChanged(e)));
-
-        public bool IsSelected
-        {
-            get => (bool)GetValue(IsSelectedProperty);
-            set => SetValue(IsSelectedProperty, value);
-        }
-
-        protected virtual void OnIsSelectedPropertyChanged(DependencyPropertyChangedEventArgs args)
-        {
-            if ((bool)args.NewValue)
-                _ = PreloadAsync();
-        }
-
-        public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(nameof(IsExpanded), typeof(bool), typeof(FolderVM),
-                new PropertyMetadata(false, (DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as FolderVM).OnIsExpandedPropertyChanged(e)));
-
-        public bool IsExpanded
-        {
-            get => (bool)GetValue(IsExpandedProperty);
-            set => SetValue(IsExpandedProperty, value);
-        }
-
-        protected virtual void OnIsExpandedPropertyChanged(DependencyPropertyChangedEventArgs args)
-        {
-            if ((bool)args.NewValue)
-                _ = PreloadAsync();
-        }
-
-        #region SubFolders Property Members
-
-        private static readonly DependencyPropertyKey InnerSubFoldersPropertyKey = DependencyProperty.RegisterReadOnly(nameof(InnerSubFolders), typeof(ObservableCollection<FolderVM>), typeof(FolderVM), new PropertyMetadata(null));
-
-        private static readonly DependencyPropertyKey SubFoldersPropertyKey = DependencyProperty.RegisterReadOnly(nameof(SubFolders), typeof(ReadOnlyObservableCollection<FolderVM>), typeof(FolderVM), new PropertyMetadata(null));
-
-        protected static readonly DependencyProperty InnerSubFoldersProperty = InnerSubFoldersPropertyKey.DependencyProperty;
-
-        public static readonly DependencyProperty SubFoldersProperty = SubFoldersPropertyKey.DependencyProperty;
-
-        protected ObservableCollection<FolderVM> InnerSubFolders
-        {
-            get => (ObservableCollection<FolderVM>)GetValue(InnerSubFoldersProperty);
-            private set => SetValue(InnerSubFoldersPropertyKey, value);
-        }
-
-        public ReadOnlyObservableCollection<FolderVM> SubFolders
-        {
-            get => (ReadOnlyObservableCollection<FolderVM>)GetValue(SubFoldersProperty);
-            private set => SetValue(SubFoldersPropertyKey, value);
-        }
-
-        #endregion
-
-        #region Files Property Members
-
-        private static readonly DependencyPropertyKey InnerFilesPropertyKey = DependencyProperty.RegisterReadOnly(nameof(InnerFiles), typeof(ObservableCollection<FileVM>), typeof(FolderVM), new PropertyMetadata(null));
-
-        private static readonly DependencyPropertyKey FilesPropertyKey = DependencyProperty.RegisterReadOnly(nameof(Files), typeof(ReadOnlyObservableCollection<FileVM>), typeof(FolderVM), new PropertyMetadata(null));
-
-        protected static readonly DependencyProperty InnerFilesProperty = InnerFilesPropertyKey.DependencyProperty;
-
-        public static readonly DependencyProperty FilesProperty = FilesPropertyKey.DependencyProperty;
-
-        protected ObservableCollection<FileVM> InnerFiles
-        {
-            get => (ObservableCollection<FileVM>)GetValue(InnerFilesProperty);
-            private set => SetValue(InnerFilesPropertyKey, value);
-        }
-
-        public ReadOnlyObservableCollection<FileVM> Files
-        {
-            get => (ReadOnlyObservableCollection<FileVM>)GetValue(FilesProperty);
-            private set => SetValue(FilesPropertyKey, value);
-        }
-
-        #endregion
-
-        private static readonly DependencyPropertyKey AccessErrorPropertyKey = DependencyProperty.RegisterReadOnly(nameof(AccessError), typeof(string), typeof(FolderVM), new PropertyMetadata(""));
-
-        public static readonly DependencyProperty AccessErrorProperty = AccessErrorPropertyKey.DependencyProperty;
-
-        public string AccessError
-        {
-            get => GetValue(AccessErrorProperty) as string;
-            private set => SetValue(AccessErrorPropertyKey, value);
-        }
-
-        private FolderVM(string name, DirectoryInfo directoryInfo)
-        {
-            _logger = App.GetLogger(this);
-            Name = name;
-            Path = directoryInfo.FullName;
-            Attributes = directoryInfo.Attributes;
-            CreationTime = directoryInfo.CreationTime;
-            LastWriteTime = directoryInfo.LastWriteTime;
-            InnerSubFolders = new();
-            SubFolders = new(InnerSubFolders);
-            InnerFiles = new();
-            Files = new(InnerFiles);
         }
 
         private void Preload(int depth, CancellationToken cancellationToken)
@@ -341,7 +373,7 @@ namespace FsInfoCat.Desktop.ViewModel
                     foreach (FolderVM folder in subFolders)
                     {
                         cancellationToken.ThrowIfCancellationRequested();
-                        folder.PreloadAsync(depth).Wait();
+                        folder.PreloadAsync(depth).Wait(cancellationToken);
                     }
                 }
                 else
@@ -364,7 +396,7 @@ namespace FsInfoCat.Desktop.ViewModel
                     foreach (FolderVM folder in subFolders)
                     {
                         cancellationToken.ThrowIfCancellationRequested();
-                        folder.PreloadAsync(depth).Wait();
+                        folder.PreloadAsync(depth).Wait(cancellationToken);
                     }
                 }
                 else
