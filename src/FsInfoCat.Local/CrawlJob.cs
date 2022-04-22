7acf13d947f5289e42e9b86aaeba1d4134ffb9cc
Leonard T. Erwine <lenny@erwinefamily.net>
2022-04-22 00:55:34 -0400

    Code cleanup

--- src/FsInfoCat.Local/CrawlJob.cs
+++ src/FsInfoCat.Local/CrawlJob.cs
@@ -2,9 +2,6 @@ using FsInfoCat.Activities;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FsInfoCat.Local
 {
