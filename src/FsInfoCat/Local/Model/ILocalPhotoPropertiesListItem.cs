33bcaaeef54791e1bd91661c989e8f7b5ccd490d
Leonard <lenny@erwinefamily.net>
2025-05-08 22:27:31 -0400

    Removed unnecessary comment doc

--- src/FsInfoCat/Local/Model/ILocalPhotoPropertiesListItem.cs
+++ src/FsInfoCat/Local/Model/ILocalPhotoPropertiesListItem.cs
@@ -4,7 +4,6 @@ namespace FsInfoCat.Local.Model
 {
     /// <summary>
     /// Generic interface for list item entities containing extended file properties for photo files.
-    /// Implements the <see cref="IPhotoPropertiesListItem" />
     /// </summary>
     /// <seealso cref="ILocalPhotoPropertiesRow" />
     /// <seealso cref="ILocalPropertiesListItem" />
