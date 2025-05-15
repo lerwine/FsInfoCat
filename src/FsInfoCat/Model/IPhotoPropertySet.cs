b876553d0863a807b81a75df24eb721645dbbf1a
Leonard <lenny@erwinefamily.net>
2025-05-15 03:08:19 -0400

    Fixed interface inheritance

--- src/FsInfoCat/Model/IPhotoPropertySet.cs
+++ src/FsInfoCat/Model/IPhotoPropertySet.cs
@@ -5,11 +5,7 @@ namespace FsInfoCat.Model
     /// <summary>
     /// Interface for database objects that contain extended file property values of photo files.
     /// </summary>
-    /// <seealso cref="IPropertySet" />
-    /// <seealso cref="IPhotoProperties" />
-    /// <seealso cref="Local.Model.ILocalPhotoPropertySet" />
-    /// <seealso cref="Upstream.Model.IUpstreamPhotoPropertySet" />
-    /// <seealso cref="IFile.PhotoProperties" />
-    /// <seealso cref="IDbContext.PhotoPropertySets" />
-    public interface IPhotoPropertySet : IPropertySet, IPhotoProperties, IEquatable<IPhotoPropertySet> { }
+    /// <seealso cref="IPhotoPropertiesListItem" />
+    /// <seealso cref="IDbContext.PhotoPropertySets"/>
+    public interface IPhotoPropertySet : IPropertySet, IPhotoPropertiesRow, IEquatable<IPhotoPropertySet> { }
 }
