12738e34105eaa348eb46766fe3de2a2b4fe55e1
Leonard <lenny@erwinefamily.net>
2025-05-31 19:40:59 -0400

    Removed unecessary warning suppression

--- src/FsInfoCat/NullableCoersion.cs
+++ src/FsInfoCat/NullableCoersion.cs
@@ -2,8 +2,6 @@ using System.Collections.Generic;
 
 namespace FsInfoCat;
 
-// TODO: Document NullableCoersion class
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 internal class NullableCoersion<T> : Coersion<T?>
     where T : struct
 {
@@ -27,5 +25,3 @@ internal class NullableCoersion<T> : Coersion<T?>
         return true;
     }
 }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
-
